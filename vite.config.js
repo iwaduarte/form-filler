158fb890631db95d1a0538521ace86a8b4a73f43
Iwá Duarte <iwaduarte@gmail.com>
2022-10-21 00:02:55 -0300

    feat: add manifest plugin to vite for manifest v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import webExtension from "@samrum/vite-plugin-web-extension";
+import manifest from "./manifest.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), webExtension({ manifest })],
+});
