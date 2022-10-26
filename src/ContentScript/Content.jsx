d8c76eaf049d9f2603bf214f97fd7e499254fec5
Iwá Duarte <iwaduarte@gmail.com>
2022-10-26 18:53:55 -0300

    feat: wait for page to load before add react rendering
    - add shadowElements and cssStyleSheet to shadow 
    - add Observer to observer changes inside react applications
    - add Outside click event handler to dismiss panel

--- src/ContentScript/Content.jsx
+++ src/ContentScript/Content.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const exampleNames = [
   { name: "First Name", value: "Iwá", match: "" },
@@ -13,11 +13,21 @@ const Content = ({ elem }) => {
   const [targetElement, setTargetElement] = useState(null);
   const [menuPositionTop, setMenuPositionTop] = useState(500);
   const [menuPositionLeft, setMenuPositionLeft] = useState(500);
+  const [reRender, setReRender] = useState(0);
+  const inputsLength = useRef(0);
+
+  const handleClickOutside = (e) => {
+    const tagName = e.target.tagName.toLowerCase();
+    if (tagName === "input" || tagName === "textarea" || !displayList)
+      return false;
+    setDisplayList(false);
+  };
 
   const handleListClick = (index) => {
     const { value } = inputs[index] || {};
     if (!targetElement || !value) return false;
     targetElement.innerHTML = value;
+    targetElement.value = value;
     setDisplayList(false);
   };
   const handleInputClick = (e) => {
@@ -26,18 +36,45 @@ const Content = ({ elem }) => {
     setMenuPositionTop(e.pageY + 5);
     setDisplayList(true);
   };
+  const handleForceRender = () => {
+    const currentInputsLength =
+      document.querySelectorAll("input,textarea").length;
 
+    if (
+      currentInputsLength > 0 &&
+      currentInputsLength >= inputsLength.current
+    ) {
+      inputsLength.current = currentInputsLength;
+      setReRender((prev) => prev + 1);
+    }
+  };
   useEffect(() => {
-    console.log("VAMOOOOOOOOOOOOOOO");
     const allInputs = Array.from(document.querySelectorAll("input,textarea"));
+    inputsLength.current = allInputs.length;
+
+    document.addEventListener("click", handleClickOutside);
     allInputs.forEach((input, index) => {
       input.addEventListener("click", handleInputClick);
     });
+
     return () => {
       allInputs.forEach((input) => {
         input.removeEventListener("click", handleInputClick);
       });
+      document.removeEventListener("click", handleClickOutside);
     };
+  }, [displayList, reRender]);
+
+  useEffect(() => {
+    const reactRoot = document.getElementById("root");
+    if (reactRoot) {
+      const observer = new MutationObserver(handleForceRender);
+      const config = { attributes: false, childList: true, subtree: true };
+      observer.observe(reactRoot, config);
+      return () => {
+        observer.disconnect();
+      };
+    }
   }, []);
 
   return displayList ? (
--- src/ContentScript/main.jsx
+++ src/ContentScript/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Content from "./Content.jsx";
-import "./index.css";
+import css from "./content.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Content />
-  </React.StrictMode>
-);
+window.onload = () => {
+  const div = document.createElement("div");
+  const shadow = div.attachShadow({ mode: "open" });
+  const CSSStyle = new CSSStyleSheet();
+
+  div.setAttribute("id", "form-filler-ext");
+  CSSStyle.replaceSync(css);
+  shadow.adoptedStyleSheets = [CSSStyle];
+  document.body.appendChild(div);
+
+  ReactDOM.createRoot(shadow).render(
+    <React.StrictMode>
+      <Content />
+    </React.StrictMode>
+  );
+};
