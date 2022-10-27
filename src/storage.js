c6ea876584fef7ebd893955e30398a50951acc3e
Iwá Duarte <iwaduarte@gmail.com>
2022-10-27 18:47:15 -0300

    feat: add storage to sync operations

---
+++ src/storage.js
@@ -0,0 +1,19 @@
+const { sync, onChanged } = chrome.storage;
+
+const setStore = (value) =>
+  new Promise((res) => {
+    sync.set({ formFiller: value }, () => {
+      res("done");
+    });
+  });
+const getFromStore = () =>
+  new Promise((res) => {
+    sync.get("formFiller", (data) => {
+      const { formFiller = [] } = data || {};
+      res(formFiller);
+    });
+  });
+
+const syncStore = (updateChanges) => onChanged.addListener(updateChanges);
+
+export { setStore, getFromStore, syncStore };
