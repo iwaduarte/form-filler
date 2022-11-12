134d7845beedf2870b9341e1f8533183e17d0387
Iwá Duarte <iwaduarte@gmail.com>
2022-11-12 11:29:29 -0300

    feat: add keyboard listener for quick iteration

--- src/ContentScript/cacheData.js
+++ src/ContentScript/cacheData.js
@@ -1,5 +1,11 @@
 const data = {
   store: {},
+  whiteList: {
+    "boards.greenhouse.io": true,
+    "angel.co": true,
+    "jobs.lever.co": true,
+  },
+  isEnabled: false,
   timeoutId: 0,
 };
 
--- src/ContentScript/main.jsx
+++ src/ContentScript/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import css from "./content.css";
 import Context from "./Context.jsx";
-import { getFromStore, syncStore } from "../storage.js";
+import { getFromStore, setStore, syncStore } from "../storage.js";
 import { defaultFiller, observeMutations } from "./automaticFiller.js";
 import * as angelList from "./Custom/angelList.js";
 import { data } from "./cacheData.js";
@@ -11,52 +11,84 @@ const siteConfiguration = {
   "angel.co": angelList,
 };
 
-const whiteList = {};
+const startOnKey = async (evt, fillForms, reactRoot, component) => {
+  const url = document.location.hostname.replace("www.", "");
 
-const startOnKey = (evt, fillForms) => {
-  if (evt.ctrlKey && evt.shiftKey && evt.key === "f") fillForms();
+  if (evt.ctrlKey && evt.altKey && evt.key === "f") {
+    if (!data.isEnabled) return;
+    console.log(url);
+    fillForms(data.store);
+    reactRoot.render(component);
+  }
+
+  if (evt.ctrlKey && evt.altKey && evt.key === "A") {
+    const storedItem = (await getFromStore("whiteList")) || [];
+    const newData = { ...storedItem, [url]: true };
+    await setStore(newData);
+    data.whiteList = { ...data.whiteList, ...newData };
+  }
 };
 
-const startApplication = async () => {
+const createShadowElement = () => {
   const div = document.createElement("div");
   const shadow = div.attachShadow({ mode: "open" });
   const CSSStyle = new CSSStyleSheet();
-
   div.setAttribute("id", "form-filler-ext");
   CSSStyle.replaceSync(css);
   shadow.adoptedStyleSheets = [CSSStyle];
   document.body.appendChild(div);
-  const url = document.location.hostname.replace("www.", "");
 
-  const { config, handleMutation, filler, watchSelector } =
-    siteConfiguration[url] || {};
-  const fillForms = filler || defaultFiller;
-  const { isEnabled, formFiller } = await getFromStore(null);
+  return shadow;
+};
 
-  document.addEventListener("keydown", (evt) => startOnKey(fillForms, evt));
+const createReactRoot = (element) => {
+  return ReactDOM.createRoot(element);
+};
 
-  if (whiteList[url]) return;
-
-  data.store = formFiller;
-
-  syncStore((changes) => {
-    const { formFiller, isEnabled: _isEnabled = true } = changes;
-    const { newValue } = formFiller || {};
-    const store = newValue || data.store;
-    data.store = store;
-    _isEnabled && fillForms(store);
-  });
-
-  if (!isEnabled) return;
-
-  fillForms(data.store);
-
-  observeMutations({ config, handleMutation, watchSelector });
-  ReactDOM.createRoot(shadow).render(
+const startApplication = async () => {
+  const shadow = createShadowElement();
+  const reactRoot = createReactRoot(shadow);
+  const component = (
     <React.StrictMode>
       <Context fields={data.store} />
     </React.StrictMode>
   );
+  const url = document.location.hostname.replace("www.", "");
+  const { config, handleMutation, filler, watchSelector } =
+    siteConfiguration[url] || {};
+  const fillForms = filler || defaultFiller;
+  const {
+    isEnabled = false,
+    formFiller = [],
+    whiteList = {},
+  } = (await getFromStore(null)) || {};
+
+  data.whiteList = { ...data.whiteList, ...whiteList };
+  data.store = formFiller;
+  data.isEnabled = isEnabled;
+
+  document.addEventListener("keydown", (evt) =>
+    startOnKey(evt, fillForms, reactRoot, component)
+  );
+
+  syncStore((changes) => {
+    const { formFiller, isEnabled: _isEnabled = true, whiteList } = changes;
+    const { newValue } = formFiller || {};
+    const store = newValue || data.store;
+    data.store = store;
+    data.whiteList = { ...data.whiteList, ...whiteList };
+    data.isEnabled = isEnabled;
+
+    _isEnabled && fillForms(store);
+  });
+
+  if (!data.whiteList[url] || !isEnabled) return;
+
+  fillForms(data.store);
+
+  observeMutations({ config, handleMutation, watchSelector });
+
+  reactRoot.render(component);
 };
 
-startApplication();
+startApplication().then();
