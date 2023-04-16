364050196b0eecc5132423061f7786743ba639a1
Iwá Duarte <iwaduarte@gmail.com>
2023-04-16 01:27:32 -0300

    feat: update README.md - feat: add dist folder to repository

---
+++ dist/assets/form-filler.9e46504d.js
@@ -0,0 +1,1 @@
+const a="/assets/arrow.aa76a0c0.svg",s="/assets/form-filler.2fbd8fba.png";export{a,s as f};
---
+++ dist/src/ContentScript/main.js
@@ -0,0 +1,1 @@
+(async()=>{await import(chrome.runtime.getURL("assets/src/ContentScript/main.aea35906.js"))})();
